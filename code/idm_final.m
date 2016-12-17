427108311f9b169b687ac5d7cfb9659be1f2e1ee
Luzian Hug <luhug@student.ethz.ch>
2016-12-17 22:20:10 +0100

    Corrected graph

--- code/idm_final.m
+++ code/idm_final.m
@@ -12,7 +12,6 @@ a = 0.3; %maximum acceleration of a car
 b = 3; %comfortable braking deceleration
 delta = 4; %exponent used in equation
 sStar = @(va,dva,dva1) s0 + va*T + va*dva/2/sqrt(a*b); %influence of the following car
-sStar2 = @(va,dva,dva1) s0 + va*T + va*dva/2/sqrt(a*b); %influence of the following car
 v0=30; %Max. speed
 
 result = zeros(length(x),1);
@@ -27,22 +26,15 @@ for ii = 1:Ncars
         dva = (x(ii+Ncars) - x(ii-1 + Ncars));
 
         sa = x(ii-1) - x(ii) - lcar;
-        %      if sa < s0
-        %          sa = s0;
-        %      end
+  
     else %First car
         dva = x(ii+Ncars);
         sa = L - x(ii);
         dva1 = 0;
-        %         if x(ii) > 803.5
-        %             sa = L - x(ii);
-        %         elseif x(ii) > 800
-        %             sa = 2.001;
-        %         end
+
     end
     
     %State Space Equation
-    %if startTimes(ii) <= t %&& sa > s0 + 0.001
     if ~guideMap(ii) %"Normal" cars
         result(ii) = x(ii+Ncars);
         result(ii+Ncars) = min(a*(1 - (x(ii+Ncars)/v0)^delta - (sStar(x(ii+Ncars),dva,dva1)/sa)^2),a);
@@ -56,11 +48,10 @@ for ii = 1:Ncars
         end
         
         if k <= 0 %if it is the first guide car
-            result(ii+Ncars) = min(a*(1 - (x(ii+Ncars)/v0)^delta - (sStar2(x(ii+Ncars),dva,dva1)/sa)^2),a);
+            result(ii+Ncars) = min(a*(1 - (x(ii+Ncars)/v0)^delta - (sStar(x(ii+Ncars),dva,dva1)/sa)^2),a);
         else
-            result(ii+Ncars) = min(a*(1 - (x(ii+Ncars)/v0)^delta - (sStar2(x(ii+Ncars),dva,dva1)/sa)^2 - (sa<triggerDis)*(x(ii+Ncars)-x(k+Ncars))),a);%(x(ii+Ncars)>x(k+Ncars))*
+            result(ii+Ncars) = min(a*(1 - (x(ii+Ncars)/v0)^delta - (sStar(x(ii+Ncars),dva,dva1)/sa)^2 - (sa<triggerDis)*(x(ii+Ncars)-x(k+Ncars))),a);%(x(ii+Ncars)>x(k+Ncars))*
         end
-        %result(ii+Ncars) = -10000*(x(ii) - (x(ii+1)+x(ii-1))/2);
         
     end
     %Introducing a disturbance
@@ -78,10 +69,6 @@ for ii = 1:Ncars
     if result(ii)<0
         result(ii) = 0;
     end
-    %     else
-    %         result(ii) = 0;
-    %         result(ii+Ncars) = 0;
-    %     end
     
 end
 
